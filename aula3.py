f990634e658e12ae99faf5926a6e5f43b80e0d65
Joyce <joyce_claine@hotmail.com>
2018-04-03 16:12:16 -0300

    ajuste de codigo

--- aula3.py
+++ aula3.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python3
+from math import sqrt as raiz
 
 def rpg2(a,b,c):
-	delta = b*b-4*a*c
-	if delta = b/(2*a) 
-	return raiz (a,b,c):
+	delta = b**2 - 4*a*c
+	print ("a = " + str(a) + " ; b = " + str(b) +" ; c = " + str(c))
+	print ("delta = " + str(delta))
+	
+	if delta < 0;
+	  return False
+	if delta == 0;
+	  return -b/(2*a)
+	if delta > 0;
+	  return (-b + raiz(delta))/(2*a), (-b - raiz(delta))/ (2*a):
+		  
+		  
 		
-		
+x = rpg2(1,2,3)			
+print(x, type(x))
+x = rpg2(-10,-10,-10)			
+print(x, type(x))
+x = rpg2(-4,4,1)			
+print(x, type(x))
+x = rpg2(-4,-4,-1)			
+print(x, type(x))		
 	
